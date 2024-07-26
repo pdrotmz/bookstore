d0af5ff52ab8c3d4f9c1fdda0acf454fdd7c4588
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-26 20:51:29 -0300

    feat(model): add AuthorModel class

---
+++ src/main/java/dev/pdrotmz/bookstore/models/AuthorModel.java
@@ -0,0 +1,36 @@
+package dev.pdrotmz.bookstore.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_AUTHOR")
+public class AuthorModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String author;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
