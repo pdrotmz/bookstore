cec1777e2caa1e9f4dafb8391a6ec3f02ab952ea
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-26 20:45:22 -0300

    feat(model): add BookModel class

---
+++ src/main/java/dev/pdrotmz/bookstore/models/BookModel.java
@@ -0,0 +1,38 @@
+package dev.pdrotmz.bookstore.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_BOOK")
+public class BookModel implements Serializable {
+
+    // Id de controle da JVM para serialização
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true) // (não aceita nulo, titulo único)
+    private String title;
+
+    // Getters e Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
